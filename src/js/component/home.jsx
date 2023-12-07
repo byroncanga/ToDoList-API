413aa4a89dd6401b21aafb2ba61f78c91e2a9b7d
byroncanga <byroncanga@gmail.com>
2023-12-07 14:46:47 -0500

    Project Ready

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleNewTask = () => {
+    const newTasks = [...tasks, inputValue];
+    setTasks(newTasks);
+    setInputValue("");
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = tasks.filter((_, idx) => idx !== index);
+    setTasks(newTasks);
+  };
+  const fechaActual = new Date().toLocaleDateString();
+
+  return (
+    <div className="containerGrid">
+      <div className="box">
+        <div className="text-center">
+          <div className="d-flex">
+            <div className="imgBox">
+              <img
+                className="imgHeader"
+                src="https://lh3.googleusercontent.com/a/ACg8ocIdw9SHZnqt4rm4UWshytYtc6Xl6Xbc5mtQBxChcbiXcGQ=s288-c-no"
+                alt=""
+              />
+            </div>
+            <div className="d-flex flex-column align-items-start mx-2 mb-3">
+              <h1 className=" text-light ">My Day</h1>
+              <p className="text-light">{fechaActual}</p>
+            </div>
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              <i className="bi bi-input-cursor-text"></i>
+            </span>
+            <input
+              name="input"
+              type="text"
+              className="form-control input"
+              placeholder="Agregar nueva tarea"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+            <button
+              type="submit"
+              className="Btn"
+              id="basic-addon1"
+              onClick={handleNewTask}
+            >
+              <div className="sign">+</div>
+              <div className="text"> Add</div>
+            </button>
+          </div>
+        </div>
+        <div>
+          {tasks.map((task, index) => (
+            <div className="d-flex align-items-center justify-content-between">
+              <div
+                className="w-100 bg-light p-2 rounded-2 my-2 mx-1"
+                key={index}
+              >
+                {task}
+              </div>
+              <div>
+                <button class="button" onClick={() => handleDeleteTask(index)}>
+                  <span class="X"></span>
+                  <span class="Y"></span>
+                  <div class="close">Close</div>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
